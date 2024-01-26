a33cd57318e50c04f507bad9cf5fb46219581bf6
parsa.javadian <parsa.javadian@yahoo.com>
2024-01-26 20:12:13 +0330

    Home Page refactor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@
     <div class="mx-auto max-w-screen-xl px-4 py-32 flex h-[calc(100vh-75px)] items-center">
     <div class="mx-auto max-w-3xl text-center">
       <h1
-        class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-4xl font-extrabold text-transparent sm:text-6xl"
+        class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-4xl font-extrabold text-transparent sm:text-7xl"
       >
         CarnismDebunked
 
