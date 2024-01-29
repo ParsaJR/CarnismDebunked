84bff2e73aa2d89df8419795331fee19a0cef2a1
parsa.javadian <parsa.javadian@yahoo.com>
2024-01-29 15:16:50 +0330

    Change the name of argument component

--- src/views/GeneralView.vue
+++ src/views/GeneralView.vue
@@ -1,12 +1,12 @@
-<script setup>
-import ArgIntroVue from '@/components/ArgIntro.vue';
+<script setup >
+import ArgIntro from '@/components/ArgIntro.vue';
 
 
 </script>
 
 <template>
     <section class="relative px-8 py-12 mx-auto md:px-12 lg:px-32 max-w-7xl lg:pb-24 lg:pt-12">
-        <ArgIntroVue></ArgIntroVue>
+        <ArgIntro></ArgIntro>
     </section>
 </template>
 
