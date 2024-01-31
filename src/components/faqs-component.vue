33d867551faa2b8c31971f68b44a6689b8008756
parsa.javadian <parsa.javadian@yahoo.com>
2024-01-31 14:33:38 +0330

    setup json file with some fake inputs

--- src/components/faqs-component.vue
+++ src/components/faqs-component.vue
@@ -1,12 +1,11 @@
 <script setup>
-// make this data dynamic with v-for and json data injection
+const generalFaqs = defineProps(['data'])
 </script>
 
 <template>
     <section class="pt-10 flex gap-y-8 flex-col p-2">
         <div class="relative">
             <label for="Search" class="sr-only"> Search for... </label>
-
             <input type="text" id="Search" placeholder="جستجو"
                 class="w-full rounded-md py-2.5 pe-10 shadow-sm border-gray-700 bg-gray-800 text-white sm:text-sm" />
 
@@ -22,10 +21,10 @@
                 </button>
             </span>
         </div>
-        <div class="space-y-4">
+        <div class="space-y-4" v-for="faq in generalFaqs.data" :key="faq.id">
             <details class="group [&_summary::-webkit-details-marker]:hidden">
                 <summary class="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-green-800 p-4">
-                    <h3 class="font-medium">چطور ممکنه که این فلان شه؟</h3>
+                    <h3 class="font-medium">{{ faq.question }}</h3>
 
                     <svg class="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-180"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -33,31 +32,10 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
                     </svg>
                 </summary>
-
                 <p class="mt-4 px-4 leading-relaxed">
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و
-                    متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
-                    کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
-                </p>
-            </details>
-
-            <details class="group [&_summary::-webkit-details-marker]:hidden">
-                <summary class="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-green-800 p-4">
-                    <h3 class="font-medium">چطور ممکنه که این فلان شه؟</h3>
-
-                    <svg class="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-180"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                    </svg>
-                </summary>
-
-                <p class="mt-4 px-4 leading-relaxed">
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و
-                    متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
-                    کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
+                    {{ faq.answer }}
                 </p>
             </details>
         </div>
-
     </section>
 </template>
--- src/views/GeneralView.vue
+++ src/views/GeneralView.vue
@@ -1,14 +1,14 @@
 <script setup >
 import ArgIntro from '@/components/ArgIntro.vue';
 import FaqsComponent from '@/components/faqs-component.vue';
-
+import generalData from '@/data/general.json'
 
 </script>
 
 <template>
     <section class="relative px-8 py-12 mx-auto md:px-12 lg:px-32 max-w-7xl lg:pb-24 lg:pt-12">
         <ArgIntro></ArgIntro>
-        <FaqsComponent></FaqsComponent>
+        <FaqsComponent :data="generalData"></FaqsComponent>
     </section>
 </template>
 
