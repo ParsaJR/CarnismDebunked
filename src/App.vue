dc4aa67ee8f72970ce384b6e865507dd2b8ada7c
parsa.javadian <parsa.javadian@yahoo.com>
2024-03-05 23:43:35 +0330

    Completing the mobile header

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ var dropDown = ref(false)
 const dropdownClass = computed(() => {
   return dropDown.value ? 'dropdownActive' : ''
 })
+const mobileDropdown = ref(false)
 
 // close dropdown if user clicked outside of dropdown
 const handleClickOutside = (event) => {
@@ -31,22 +32,60 @@ onMounted(() => {
   <div class="flex flex-col min-h-screen">
     <!-- Mobile Header -->
     <header class="sm:hidden flex p-7">
-      <button @click="openNav()" class="flex flex-col justify-center items-center">
-        <svg viewBox="0 0 100 80" width="40" height="40" fill="#dedede">
-          <rect width="100" height="20" rx="8"></rect>
-          <rect y="30" width="100" height="20" rx="8"></rect>
-          <rect y="60" width="100" height="20" rx="8"></rect>
-        </svg>
-        فهرست
-      </button>
+      <div class="grid grid-cols-2 gap-4 w-full">
+        <div class="flex text-center flex-col justify-center items-start">
+          <RouterLink class="cursor-pointer" to="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-10 h-10"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+            <p>خانه</p>
+          </RouterLink>
+        </div>
+        <div class="flex justify-end">
+          <div>
+            <button @click="openNav()" class="flex flex-col justify-center items-center">
+              <svg viewBox="0 0 100 80" width="40" height="40" fill="#dedede">
+                <rect width="100" height="10" rx="8"></rect>
+                <rect y="30" width="100" height="10" rx="8"></rect>
+                <rect y="60" width="100" height="10" rx="8"></rect>
+              </svg>
+              فهرست
+            </button>
+          </div>
+        </div>
+      </div>
       <!--grey overlay-->
       <div id="myNav" class="overlay">
         <a class="closebtn cursor-pointer" @click="closeNav()">&times;</a>
         <div class="overlay-content">
-          <a href="#">لینک اول</a>
-          <a href="#">لینک دوم</a>
-          <a href="#">لینک سوم</a>
-          <a href="#">لینک چهارم</a>
+          <RouterLink to="/" class="text-3xl" @click="closeNav()">خانه</RouterLink>
+          <a class="text-3xl cursor-pointer" @click="mobileDropdown = !mobileDropdown"
+            >استدلال ها +</a
+          >
+          <ul class="bg-[#414141] p-5" :class="{ hidden: mobileDropdown == false }">
+            <li class="text-2xl my-1">
+              <RouterLink to="general" @click="closeNav()">عمومی / اخلاقی</RouterLink>
+            </li>
+            <li class="text-2xl my-1">
+              <RouterLink to="health" @click="closeNav()">تغذیه</RouterLink>
+            </li>
+            <li class="text-2xl my-1">
+              <RouterLink to="environment" @click="closeNav()">محیط زیست</RouterLink>
+            </li>
+          </ul>
+          <RouterLink to="About" class="text-3xl" @click="closeNav()">درباره ما</RouterLink>
+          <a href="#" class="text-3xl">تماس با ما</a>
         </div>
       </div>
     </header>
@@ -54,31 +93,37 @@ onMounted(() => {
     <header class="w-full text-center p-6 hidden sm:block">
       <div class="header-items">
         <!-- bg-[#2e2e2e] -->
-        <RouterLink to="/" class="link-underline-animate">خانه</RouterLink>
+        <RouterLink to="/" class="link-underline-animate text-2xl">خانه</RouterLink>
         <div class="inline-block dropdown">
           <a
-            class="mr-8 ml-8 hover:cursor-pointer select-none"
+            class="mr-8 ml-8 hover:cursor-pointer select-none text-2xl"
             to="/"
             v-on:click="dropDown = !dropDown"
           >
             + استدلال ها</a
           >
           <div class="dropdown-content rounded-2xl" :class="dropdownClass">
-            <RouterLink class="hover:text-primary" v-on:click="dropDown = !dropDown" to="general"
+            <RouterLink
+              class="hover:text-primary text-xl"
+              v-on:click="dropDown = !dropDown"
+              to="general"
               >عمومی</RouterLink
             >
-            <RouterLink class="hover:text-primary" v-on:click="dropDown = !dropDown" to="health"
+            <RouterLink
+              class="hover:text-primary text-xl"
+              v-on:click="dropDown = !dropDown"
+              to="health"
               >تغذیه</RouterLink
             >
             <RouterLink
-              class="hover:text-primary"
+              class="hover:text-primary text-xl"
               v-on:click="dropDown = !dropDown"
               to="environment"
               >محیط زیست
             </RouterLink>
           </div>
         </div>
-        <RouterLink to="About" class="link-underline-animate">درباره ما</RouterLink>
+        <RouterLink to="About" class="link-underline-animate text-2xl">درباره ما</RouterLink>
       </div>
     </header>
     <main class="flex-grow flex">
--- src/components/FaqFooter.vue
+++ src/components/FaqFooter.vue
@@ -3,5 +3,8 @@ import seperator from './separator-component.vue'
 </script>
 <template>
   <seperator></seperator>
-  <p class="text-center pt-10 text-lg"> هنوز سوالی دارید؟  <a href="" class="underline transition hover:text-primary">برای ما ارسال کنید</a></p>
+  <p class="text-center pt-10 text-lg">
+    هنوز سوالی دارید؟
+    <a href="" class="underline transition hover:text-primary">برای ما ارسال کنید</a>
+  </p>
 </template>
