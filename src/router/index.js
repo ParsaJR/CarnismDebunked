4ba5b7896a9d443e6e1109aafd7ddf49a543f4ae
parsa.javadian <parsa.javadian@yahoo.com>
2024-03-11 19:10:30 +0330

    Add helath and environment section

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import GeneralView from '../views/GeneralView.vue'
+import HealthView from '../views/HealthView.vue'
+import EnvView from '../views/EnvView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +17,16 @@ const router = createRouter({
       path: '/general',
       name: 'General',
       component: GeneralView
+    },
+    {
+      path: '/health',
+      name: 'Health',
+      component: HealthView
+    },
+    {
+      path: '/environment',
+      name: 'Environment',
+      component: EnvView
     },
     {
       path: '/about',
