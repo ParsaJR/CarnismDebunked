26019a69be386f9049161a665df9ed43c3ecee2d
ParsaJR <parsajr@proton.me>
2024-09-01 11:35:44 +0330

    update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,21 +3,17 @@
   <div class="container mx-auto px-8 py-12 flex items-center">
     <div class="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-16">
       <section class="flex justify-center">
-        <img
-          src="../assets/img/undraw_conversation_re_c26v.svg"
-          alt="image"
-          class="object-cover sm:w-3/5 md:w-3/5 lg:w-full"
-        />
+        <img src="../assets/img/undraw_conversation_re_c26v.svg" alt="image"
+          class="object-cover sm:w-3/5 md:w-3/5 lg:w-full" />
       </section>
       <section class="mt-10 lg:mt-0 flex flex-col justify-center">
         <h3 class="leading-9">
-          این وب سایت میکوشد تا به استدلال های پرتکرار علیه گیاه خواری پاسخ دهد . مردم تصمیم
-          می‌گیرند که تصورات غلط را در مورد گیاهخواری به چالش نکشند و این وب سایت میتواند به عنوان
-          منبعی برای تقویت گفتمان سازنده ، یا رسیدن به حقیقتِ موضوع مورد استفاده قرار گیرد . <br />
+          گیاهخواری هم مانند هر نگرش و ایده‌ای مخالفان خود را دارد و هستند افرادی که وگانیسم/گیاهخواری را از ابعاد
+          مختلف به چالش می‌کشند.
+          این وب سایت میکوشد تا به استدلال های پرتکرار پیرامون این موضوع پاسخ دهد.
+          <br>
           ایده این وب سایت از
-          <a class="underline transition hover:text-primary" href="https://www.carnismdebunked.com/"
-            >CarnismDebunked</a
-          >
+          <a class="underline transition hover:text-primary" href="https://www.carnismdebunked.com/">CarnismDebunked</a>
           گرفته شده است .
         </h3>
       </section>
