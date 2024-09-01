6052f1773ce4dbcc980c2165f09e03d65e85adac
ParsaJR <parsajr@proton.me>
2024-09-01 10:10:13 +0330

    removing redundent tags

--- src/components/faqs-component.vue
+++ src/components/faqs-component.vue
@@ -66,19 +66,6 @@ await ParseMarkdowns();
     </div>
     <div class="space-y-4" v-for="(faq, index) in filteredFaqs" :key="index">
       <FaqComponent :question=faq.attributes.question :answer=faq.body></FaqComponent>
-      <!-- <details class="group [&_summary::-webkit-details-marker]:hidden">
-        <summary class="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-gray-600 p-4">
-          <h4 class="font-medium select-none">{{ faq.attributes.question }}</h4>
-
-          <svg class="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-180"
-            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            >
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-          </svg>
-        </summary>
-        <p class="mt-4 px-4 leading-relaxed whitespace-pre-wrap" v-if="faq.body" v-html="faq.body">
-        </p>
-      </details> -->
     </div>
   </section>
   <FaqFooter class="mt-20"></FaqFooter>
